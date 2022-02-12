23a7b7501333b007d36fd7f5b188de75b9416ad7
BugOrInfernal <xyzcode1972@outlook.com>
2022-02-13 02:16:51 +0800

    updata

--- src/main/java/com/example/managementsystem/ManagementSystemApplication.java
+++ src/main/java/com/example/managementsystem/ManagementSystemApplication.java
@@ -2,7 +2,7 @@ package com.example.managementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*启动类*/
 @SpringBootApplication
 public class ManagementSystemApplication {
 
